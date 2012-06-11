dc77a85f509f2b9d395712919d444d54db09c6ee
Kristof Mattei <kristof@kristofmattei.be>
2012-06-11 18:53:05 +0200

    Initial commit

---
+++ TestAppForComDay2012/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAppForComDay2012
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hi comday 2012");
+		}
+	}
+}
